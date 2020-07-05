34c836028e83749d1e8c1d67127ecae16164dee2
Yisrael Grimes <yisraelgrimes@gmail.com>
2020-07-05 16:10:01 -0500

    Added seeder file for 'people' table too.
    With groups being populated, the people table felt a little empty.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(PeopleTableSeeder::class );
+        $this->call(GroupsTableSeeder::class );
     }
 }
